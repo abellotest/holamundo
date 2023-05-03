538b2298264ff4c1166ec15d1d86bd94492a6459
abellotest <leyvaobregonabel@gmail.com>
2023-05-03 17:26:37 -0500

    con formularios y commits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,12 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'cliente-add-edit', //redirige a la pagina cliente
     pathMatch: 'full'
+  },
+  {
+    path: 'cliente-add-edit',
+    loadChildren: () => import('./cliente/cliente-add-edit/cliente-add-edit.module').then( m => m.ClienteAddEditPageModule)
   },
 ];
 
